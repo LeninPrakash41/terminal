0d44b34f3536a96fad33caefac01b3c1fa5ea1cd
Espen Andersen <post@espenandersen.no>
2013-03-06 21:40:31 +0100

    Tests for windowManager.

---
+++ test/test.windowmanager.mocha.js
@@ -0,0 +1,10 @@
+describe( "windowManager", function() {
+
+    it( "should exist", function() {
+
+        assert.isFunction( $, "hey!? where's jQuery?" );
+        assert.isFunction( $.fn.windowManager, "no windowManager plugin in jQuery object" );
+
+    } );
+
+} );
